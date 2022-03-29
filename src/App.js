46859af236f04e36f67b7831c770ba735fe1d424
Alex <73483900+TverdovskyiAleksey@users.noreply.github.com>
2022-03-29 07:33:42 +0300

    new Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Form from "./Components/Form";
+import Payment from "./Components/Payment";
+
+function App() {
+  return (
+    <div className="App">
+      12
+      <Form />
+      <Payment />
+    </div>
+  );
+}
+
+export default App;
